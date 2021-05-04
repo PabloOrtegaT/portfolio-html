e5ce1025b2fc6ffd4987a0d49137e813190a7638
PabloOrtegaT <ortegapablo96@gmail.com>
2021-05-03 19:22:15 -0500

    added new functionalities to skill bar and modified styles

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
-const marqueeContent = document.querySelector("ul.marquee-content");
+// this is my slider
+const slider = document.querySelector('.marquee-content');
 
-root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+  
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove',(e) =>{
+  if(!isDown) return;
+  e.preventDefault();
+  const x =e.pageX - slider.offsetLeft;
+  const walk = (x -startX)*3;
+  slider.scrollLeft = scrollLeft - walk;
+  slider.scrollWidth = scrollLeft - walk;
+  console.log(walk)
+});
+
+
+root.style.setProperty("--marquee-elements", slider.children.length);
 
 for(let i=0; i<marqueeElementsDisplayed; i++) {
-  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+  slider.appendChild(slider.children[i].cloneNode(true));
 }
+
