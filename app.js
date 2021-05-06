bb0f2aacdb3eae75f9af72dfc587b2ae454dcce3
PabloOrtegaT <ortegapablo96@gmail.com>
2021-05-06 14:33:51 -0500

    changed interface

--- app.js
+++ app.js
@@ -1,42 +1,82 @@
-const root = document.documentElement;
-const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
-// this is my slider
-const slider = document.querySelector('.marquee-content');
+// const root = document.documentElement;
+// const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 
-let isDown = false;
+// this is my slider
+// const marqueeContent = document.querySelector("ul.marquee-content");
+// const slider = document.querySelector('.marquee');
+
+const interval = 2500;
+
+document.addEventListener('DOMContentLoaded', function() {
+  var stream = document.querySelector('.gallery__stream');
+  var items = document.querySelectorAll('.slider');
+  var prev = document.querySelector('.gallery__prev');
+  
+  prev.addEventListener('click', function() {
+    clearInterval(slideId);
+    items = document.querySelectorAll('.slider');
+    stream.insertBefore(items[items.length - 1], items[0]);
+    startSlide();
+  });
+  
+  var next = document.querySelector('.gallery__next');
+  next.addEventListener('click', function() {
+    items = document.querySelectorAll('.slider');
+    stream.appendChild(items[0]);
+    clearInterval(slideId);
+    startSlide();
+  });
+
+  const startSlide = () => {
+    slideId = setInterval(() => {
+      items = document.querySelectorAll('.slider');
+      stream.appendChild(items[0]);
+    }, interval);
+  };
+
+  stream.addEventListener('mouseenter', () => {
+    clearInterval(slideId)
+  });
+
+  stream.addEventListener('mouseleave', startSlide);
+
+  startSlide();
+
+});
+
+/*let isDown = false;
 let startX;
 let scrollLeft;
 
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
-  slider.classList.add('active');
+  marqueeContent.classList.add('active');
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
   
 });
+
 slider.addEventListener('mouseleave', () => {
   isDown = false;
-  slider.classList.remove('active');
+  marqueeContent.classList.remove('active');
 });
+
 slider.addEventListener('mouseup', () => {
   isDown = false;
-  slider.classList.remove('active');
+  marqueeContent.classList.remove('active');
 });
 
 slider.addEventListener('mousemove',(e) =>{
   if(!isDown) return;
   e.preventDefault();
   const x =e.pageX - slider.offsetLeft;
-  const walk = (x -startX)*3;
+  const walk = (x -startX);
   slider.scrollLeft = scrollLeft - walk;
-  slider.scrollWidth = scrollLeft - walk;
-  console.log(walk)
 });
 
 
-root.style.setProperty("--marquee-elements", slider.children.length);
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 
 for(let i=0; i<marqueeElementsDisplayed; i++) {
-  slider.appendChild(slider.children[i].cloneNode(true));
-}
-
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}*/
